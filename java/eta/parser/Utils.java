80578aa3fe340d918f53c8fd384a98d5c852f6b5
Cary Robbins <carymrobbins@gmail.com>
2017-11-30 01:07:10 -0600

    Fixed memcmp implementation

--- java/eta/parser/Utils.java
+++ java/eta/parser/Utils.java
@@ -1,11 +1,14 @@
 package eta.parser;
 
+import eta.runtime.io.MemoryManager;
 import java.nio.ByteBuffer;
 
 public class Utils {
 
-  // Copied from https://github.com/typelead/eta-hackage/blob/535f422cb4a6edfa6c1e4d7486b69e6684ddd9e9/patches/bytestring-0.10.8.2.patch#L467
-  public static int memcmp(ByteBuffer b1, ByteBuffer b2, int n) {
+  // Copied from https://github.com/typelead/eta-hackage/blob/master/patches/bytestring-0.10.8.1.patch#L754
+  public static int memcmp(long address1, long address2, int n) {
+        ByteBuffer b1 = MemoryManager.getBoundedBuffer(address1);
+        ByteBuffer b2 = MemoryManager.getBoundedBuffer(address2);
         /* TODO: Is the & 0xFF required? */
         while (n-- != 0) {
             int a = b1.get() & 0xFF;
